0b4d49e1620376ecec4aa2d623c058332f9f9f03
idarshan181 <ddpatel97@gmail.com>
2020-03-17 15:39:55 +0530

    reselector

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -1,7 +1,7 @@
 import CartActionTypes from "./cart.types"
 
 
-export const toggleCartHidden = () =>({
+export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
 
